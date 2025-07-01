d0a9055900666382f68a663d8085ebe9a5593ea7
sped17 <sped17ja@gmail.com>
2025-07-01 21:29:44 +0200

    Fix: poprawa literowki

--- kalkulator.py
+++ kalkulator.py
@@ -1,12 +1,12 @@
 def dodawanie(x,y):
-    return x+y
+    return (x+y)
 
 def odejmowanie(x,y):
-    return x-y
+    return (x-y)
 def mnozenie(x,y):
-    return x*y
+    return (x*y)
 
 def dzielenie(x,y):
     if y == 0:
         return "Blad! Nie mozna dzielic przez 0!"
-    return x/y
+    return (x/y)
