e882b1fbb2d2d725115f1ffd4f2eb4afe87afdde
sped17 <sped17ja@gmail.com>
2025-07-01 20:09:08 +0200

    Feat: zdefiniowano podstawową strukture menu

--- kalkulaotr.py
+++ kalkulaotr.py
@@ -1,5 +1,5 @@
 def dodawanie(x,y):
     return x+y
 
-def odejmowanie(x,y)
+def odejmowanie(x,y):
     return x-y
--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-import kalkulaotr.py as kalk
+import kalkulaotr as kalk
+import os
+
 def menu():
-    x = input("Wprowadź pierwszą liczbę: ")
-    y = input("Wprowadź drugą liczbę: ")
-    wybor = input("Wybierz operacje matematyczną jaką chcesz wykonać")
-    print("--------------------------------------------------------------------")
-    
+    while True:
+        x = float(input("Wprowadź pierwszą liczbę: "))
+        y = float(input("Wprowadź drugą liczbę: "))
+        print("\n")
+        print("--------------------------------------------------------------------")
+        print("1. Dodawanie")
+        print("2. Odejmowanie")
+        print("3. Mnożenie")
+        print("4. Dzielenie")
+        print("9. Wyczyść ekran")
+        print("0. Wyjdź z programu")
+        print("--------------------------------------------------------------------")
+        wybor = input("Wybierz operacje matematyczną jaką chcesz wykonać: ")
+        while True:
+            match wybor:
+                case "1":
+                    print("Wynik: ", kalk.dodawanie(x,y))
+                    break
+                case "2":
+                    print("Wynik: ", kalk.odejmowanie(x,y))
+                    break
+                case "3":
+                    #print("Wynik: ", kalk.mnozenie())
+                    ...
+                case "4":
+                    #print("Wynik: ", kalk.dzielenie())
+                    ...
+                case "9":
+                    os.system("cls")
+                case "0":
+                    return 0
+                case _:
+                    print("Nie poprawny wybor!")
+                    wybor = input("Wybierz operacje jeszcze raz: ")
+                    continue
+        
 
 def main():
-    ...
+    menu()
 if __name__ == "__main__":
     main()    
