9f1a9f438f29ff84fec8ba29490526c94435b340
sped17 <sped17ja@gmail.com>
2025-07-01 19:31:56 +0200

    Feat: zainicjalizowano strukture pliku main. plik aktualnie nie dziala prawidlowo

---
+++ main.py
@@ -0,0 +1,12 @@
+import kalkulaotr.py as kalk
+def menu():
+    x = input("Wprowadź pierwszą liczbę: ")
+    y = input("Wprowadź drugą liczbę: ")
+    wybor = input("Wybierz operacje matematyczną jaką chcesz wykonać")
+    print("--------------------------------------------------------------------")
+    
+
+def main():
+    ...
+if __name__ == "__main__":
+    main()    
