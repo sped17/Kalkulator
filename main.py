f78ca8e5ac243d029b71ea3147d2fb2fada8f51a
sped17 <sped17ja@gmail.com>
2025-07-02 18:32:17 +0200

    Fix: zmieniono dzialanie aplikacji - aplikacja wykonuje sie tylko raz, tak zeby poprawnie przetestowac obraz Dockera

--- main.py
+++ main.py
@@ -2,41 +2,41 @@ import kalkulator as kalk
 import os
 
 def menu():
+    
+    x = float(input("Wprowadź pierwszą liczbę: "))
+    y = float(input("Wprowadź drugą liczbę: "))
+    print("\n")
+    print("--------------------------------------------------------------------")
+    print("1. Dodawanie")
+    print("2. Odejmowanie")
+    print("3. Mnożenie")
+    print("4. Dzielenie")
+    print("9. Wyczyść ekran")
+    print("0. Wyjdź z programu")
+    print("--------------------------------------------------------------------")
+    wybor = input("Wybierz operacje matematyczną jaką chcesz wykonać: ")
     while True:
-        x = float(input("Wprowadź pierwszą liczbę: "))
-        y = float(input("Wprowadź drugą liczbę: "))
-        print("\n")
-        print("--------------------------------------------------------------------")
-        print("1. Dodawanie")
-        print("2. Odejmowanie")
-        print("3. Mnożenie")
-        print("4. Dzielenie")
-        print("9. Wyczyść ekran")
-        print("0. Wyjdź z programu")
-        print("--------------------------------------------------------------------")
-        wybor = input("Wybierz operacje matematyczną jaką chcesz wykonać: ")
-        while True:
-            match wybor:
-                case "1":
-                    print("Wynik: ", kalk.dodawanie(x,y))
-                    break
-                case "2":
-                    print("Wynik: ", kalk.odejmowanie(x,y))
-                    break
-                case "3":
-                    print("Wynik: ", kalk.mnozenie(x,y))
-                    break
-                case "4":
-                    print("Wynik: ", kalk.dzielenie(x,y))
-                    break
-                case "9":
-                    os.system("cls")
-                case "0":
-                    return 0
-                case _:
-                    print("Nie poprawny wybor!")
-                    wybor = input("Wybierz operacje jeszcze raz: ")
-                    continue
+        match wybor:
+            case "1":
+                print("Wynik: ", kalk.dodawanie(x,y))
+                break
+            case "2":
+                print("Wynik: ", kalk.odejmowanie(x,y))
+                break
+            case "3":
+                print("Wynik: ", kalk.mnozenie(x,y))
+                break
+            case "4":
+                print("Wynik: ", kalk.dzielenie(x,y))
+                break
+            case "9":
+                os.system("cls")
+            case "0":
+                return 0
+            case _:
+                print("Nie poprawny wybor!")
+                wybor = input("Wybierz operacje jeszcze raz: ")
+                continue
         
 
 def main():
